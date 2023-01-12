289052cef458e8a93a3386a22c3c94e072b3929c
X. Tang <x.long@infobeyondtech.com>
2023-01-12 05:32:48 -0500

    Missed files

---
+++ src/PolpAbp.Presentation.Account.Web/Etos/EmailActivationSuccessEto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.EventBus;
+
+namespace PolpAbp.Presentation.Account.Web.Etos
+{
+    [EventName(nameof(EmailActivationSuccessEto))]
+    public class EmailActivationSuccessEto
+    {
+        public Guid? TenantId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
