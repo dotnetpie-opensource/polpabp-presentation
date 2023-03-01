8eccef1a8940faad9763ba4065053ec83826b5f7
Tom Tang <polpware@gmail.com>
2023-03-01 07:00:39 -0500

    Authorization success page

---
+++ src/PolpAbp.Presentation.Account.Web/Pages/Account/AuthorizeSuccess.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PolpAbp.Presentation.Account.Web.Pages.Account
+{
+    public class AuthorizeSuccessModel : PolpAbpAccountPageModel
+    {
+        public bool IsCordovaEnabled { get; set; }
+
+        public virtual async Task<IActionResult> OnGetAsync()
+        {
+            await LoadSettingsAsync();
+            return Page();
+        }
+
+        protected override async Task LoadSettingsAsync()
+        {
+            await base.LoadSettingsAsync();
+
+            IsCordovaEnabled = Configuration.GetValue<bool>("PolpAbp:Framework:IsCordovaEnabled");
+        }
+    }
+}
