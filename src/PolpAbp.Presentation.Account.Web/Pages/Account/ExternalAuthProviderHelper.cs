62d831dbd13376da8fbc7bb7b5364137aee3f6b2
Tom T <polpware@gmail.com>
2024-05-15 08:37:39 -0400

    Sign up with SSO

---
+++ src/PolpAbp.Presentation.Account.Web/Pages/Account/ExternalAuthProviderHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace PolpAbp.Presentation.Account.Web.Pages.Account
+{
+    public static class ExternalAuthProviderHelper
+    {
+        public static string GetPrettySsoSignUpName(string provider)
+        {
+            return provider switch { 
+                 "AzureAd" => "Sign Up with Microsoft",
+                 _ => provider
+            };
+        }
+    }
+}
